aaade5ff4d007be8ba4c1d794a330829d33a45d3
George Letsos <it1017@uom.edu.gr>
2019-03-26 23:26:09 +0200

    Created a Job class for ThreadWorker's jobs.

---
+++ server/workers/threads/Job.js
@@ -0,0 +1,11 @@
+/** Class representing a web worker instance of a thread. */
+module.exports = class Job {
+  /**
+   * @param {String} op The operation name.
+   * @param {Function} callback The function to be called when done.
+   */
+  constructor(op, callback) {
+    this.op = op;
+    this.callback = callback;
+  }
+};
